9ddbb2a133add3a3e55b7120b67ede6461d4f99c
hans.arias <hearias6@misena.edu.co>
2020-05-21 14:37:47 -0500

    configurar el servidor eureka

---
+++ src/test/java/com/citas/eureka/SistemaCitasEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citas.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaCitasEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
